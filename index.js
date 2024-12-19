4f538b573141433fb4f976f63f4970c0f5934200
Jonas Kaufmann <jonas.kaufmann@sunrise.ch>
2024-12-19 15:06:16 +0100

    added linting errors to test CI

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ console.log(`The ${chalk.greenBright(word1)} is ${chalk.redBright(word2)}.`);
 export const sum = (a, b) => a + b;
 
 // ESLint errors
-/*
+
 var unusedVar = 1;
 if (word1 == word2) {
   console.log(undefinedVariable);
 }
-*/
+
